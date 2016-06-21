36e8d3a0b91f23032f256f461b525d12361c2c52
Mikhail <miheyego@gmail.com>
2016-06-21 13:59:05 +0200

    minor style fixes, new feature: toggle button

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-var buttonArrow = document.getElementById('button-press');
 
-var slide1 = document.getElementById('slide-1'),
-    slide2= document.getElementById('slide-2'),
-    buttonText = document.getElementById('button-text');
+// Slider variables
+let buttonArrow = document.getElementById('button-press');
 
-let textForward = 'WEITER',
-    textBack    = 'ZURUCK';
+let slide1 = document.getElementById('slide-1');
+let slide2= document.getElementById('slide-2');
+let buttonText = document.getElementById('button-text');
 
-var defaultState = true;
+let textForward = 'WEITER';
+let textBack    = 'ZURUCK';
 
+let defaultState = true;
 
+// Accordion variables
+let toggleButtons = document.getElementsByClassName('accordion-toggle');
+let i;
+
+// Slider events
 buttonArrow.onclick = function() {
     slideMove(defaultState, changeState(this, defaultState));
 };
 
+// Accordion events
+for (i = 0; i<toggleButtons.length; i++) {
+  toggleButtons[i].onclick = function() {
+      toggleClass(this, 'show');
+      toggleClass(this.nextElementSibling, 'hidden');
+  }
+}
 
+// Helper functions
 function slideMove(defaultDir, callback) {
   if (defaultDir) {
     addClass(slide1, 'left');
@@ -57,4 +71,20 @@ function removeClass(el, cssClass) {
     el.classList.remove(cssClass);
   else
     el.cssClass = el.cssClass.replace(new RegExp('(^|\\b)' + cssClass.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
+
+function toggleClass(el, cssClass) {
+  if (el.classList) {
+  el.classList.toggle(cssClass);
+  } else {
+    var classes = el.cssClass.split(' ');
+    var existingIndex = classes.indexOf(cssClass);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(cssClass);
+
+    el.cssClass = classes.join(' ');
+  }
 }
